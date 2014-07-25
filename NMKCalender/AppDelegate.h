014d8570a0e7c975673f357132e66b88c88d0ebf
Nithin M K <nithin9mk@gmail.com>
2014-07-25 09:54:34 +0530

    Initial Commit

---
+++ NMKCalender/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NMKCalender
+//
+//  Created by NewageSMB on 7/25/14.
+//  Copyright (c) 2014 NewAgeSMB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NMKCalender/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  NMKCalender
+//
+//  Created by NewageSMB on 7/25/14.
+//  Copyright (c) 2014 NewAgeSMB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
